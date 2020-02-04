0e5eaea4f6ffe0ff6ae64b48e0f600d3d6dbf7d0
김석용 <fms2014@naver.com>
2020-02-04 18:10:53 +0900

    [Add] 지역 검색 기능 추가, 태그 입력 기능 추가 [Modify] 지도 조작 도구 수정 [Add] 주석 생성 기능 추가 S02P13D102-70 S02P13D102-94

--- front-sk/src/views/main/WriteForm.vue
+++ front-sk/src/views/main/WriteForm.vue
@@ -3,38 +3,50 @@
         <InputForm id="content-title" :enterInput="test1" :errorText="error.title" placeholder="제목을 입력해 주세요." label="제목" />
         <div class="select-form">
             <span class="select-form-title">장소</span>
-            <div class="selected">
-                <div class="hashtag-form" v-for="area in areas" :key="area">{{ area }}<button class="hashtag-cansle">×</button></div>
+            <div class="select-tag">
+                <div class="hashtag-form" v-for="area in areas" :key="area">
+                    {{ area }}<button class="hashtag-cansle" @click="removeHash(area, 'area')">×</button>
+                </div>
             </div>
         </div>
         <div class="search">
-            <input class="search-bar" type="text" />
+            <input v-model="areaTag" class="search-bar" type="text" @keyup.enter="createHash('area')" />
             <div class="search-result-dropdown">
                 <div class="dropdown-list" v-for="resultArea in resultAreas" :key="resultArea">{{ resultArea }}</div>
             </div>
         </div>
         <div class="select-form">
             <span class="select-form-title">키워드</span>
-            <div class="selected">
-                <div class="hashtag-form" v-for="keyword in keywords" :key="keyword">{{ keyword }}<button class="hashtag-cansle">×</button></div>
+            <div class="select-tag">
+                <div class="hashtag-form" v-for="keyword in keywords" :key="keyword">
+                    {{ keyword }}<button class="hashtag-cansle" @click="removeHash(keyword, 'keyword')">×</button>
+                </div>
             </div>
         </div>
         <div class="search">
-            <input class="search-bar" type="text" />
+            <input v-model="keywordTag" class="search-bar" type="text" @keyup.enter="createHash('keyword')" />
         </div>
         <div id="map-wrapper">
             <div id="map"></div>
-            <div id="insert-comment-wrap">
-                <span id="insert-comment-wrap-title">설명 넣기</span>
-                <label for="comment-title-insert">제목 <input id="comment-title-insert" type="text" v-model="commentTitle"/></label>
-                <label for="comment-content-insert">내용<textarea id="comment-content-insert" type="text" v-model="commentContent"/></label>
-                <button id="comment-submit" @click="insertComment">주석 생성</button>
+            <div id="edit">
+                <button class="draw-tools" @click="selectDrawTools('MARKER', $event)">마커</button>
+                <button class="draw-tools" @click="selectDrawTools('POLYLINE', $event)">선</button>
+                <button class="draw-tools" @click="selectDrawTools('ARROW', $event)">화살표선</button>
+                <button class="draw-tools" @click="selectDrawTools('RECTANGLE', $event)">사각형</button>
+                <button class="draw-tools" @click="selectDrawTools('CIRCLE', $event)">원</button>
+                <button class="draw-tools" @click="selectDrawTools('ELLIPSE', $event)">타원</button>
+                <button class="draw-tools" @click="selectDrawTools('POLYGON', $event)">다각형</button>
+                <div id="work">
+                    <button id="undo" class="draw-tools disabled" onclick="undo()" disabled>실행취소</button>
+                    <button id="redo" class="draw-tools disabled" onclick="redo()" disabled>원래대로</button>
+                </div>
+                <div id="insert-comment-wrap" class="drop-up">
+                    <span id="insert-comment-wrap-title">설명 넣기</span>
+                    <label for="comment-title-insert">제목 <input id="comment-title-insert" type="text" v-model="commentTitle"/></label>
+                    <label for="comment-content-insert">내용<textarea id="comment-content-insert" type="text" v-model="commentContent"/></label>
+                    <button id="comment-submit" @click="createDraw">생성</button>
+                </div>
             </div>
-
-            <p class="edit">
-                <button id="undo" class="disabled" onclick="undo()" disabled>실행취소</button>
-                <button id="redo" class="disabled" onclick="redo()" disabled>원래대로</button>
-            </p>
             <br />
             <button onclick="extractionImg()">이미지 생성</button>
         </div>
@@ -55,7 +67,6 @@
             <label for="calendar-days"><input id="calendar-days" name="calendar-days" class="calendar" type="number" /><span>일</span></label>
         </div>
         <button @click="submit">제출</button>
-        <button @click="getMapData">test</button>
     </div>
 </template>
 
@@ -69,9 +80,27 @@ export default {
     },
     data() {
         return {
-            areas: ['장소 #1', '장소 #2', '장소 #3', '장소 #4', '장소 #5', '장소 #6'],
+            areaTag: '',
+            keywordTag: '',
+            selectDraw: '',
+            areaData: [
+                '장소 #1',
+                '장소 #2',
+                '장소 #3',
+                '장소 #4',
+                '장소 #5',
+                '장소 #6',
+                '가',
+                '가나',
+                '가나다',
+                '가나다라',
+                '가나다라마',
+                '가나다라마바',
+                '가나다라마바사',
+            ],
+            areas: [],
             keywords: ['키워드 #1', '키워드 #2', '키워드 #3', '키워드 #4', '키워드 #5'],
-            resultAreas: ['결과 #1', '결과 #2', '결과 #3', '결과 #4', '결과 #5'],
+            resultAreas: [],
             imageArr: [],
             imageArrBase64: [],
             commentTitle: '',
@@ -81,9 +110,70 @@ export default {
             },
         };
     },
+    watch: {
+        areaword: function(v) {
+            this.findArea();
+        },
+        hashTag: function() {},
+    },
     methods: {
-        test1($event, type) {
-            console.log($event);
+        createDraw() {
+            selectOverlay(this.selectDraw, this.commentTitle, this.commentContent);
+            this.commentTitle = this.commentContent = '';
+        },
+        selectDrawTools(s, e) {
+            var len = document.getElementsByClassName('draw-tools').length;
+            if (e.target && !e.target.classList.contains('selected')) {
+                for (var i = 0; i < len; i++) {
+                    document.getElementsByClassName('draw-tools')[i].classList.remove('selected');
+                }
+                e.target.classList.add('selected');
+                document.getElementById('insert-comment-wrap').classList.add('drop-down');
+                document.getElementById('insert-comment-wrap').classList.remove('drop-up');
+            } else if (e.target && e.target.classList.contains('selected')) {
+                e.target.classList.remove('selected');
+                document.getElementById('insert-comment-wrap').classList.remove('drop-down');
+                document.getElementById('insert-comment-wrap').classList.add('drop-up');
+            }
+            this.selectDraw = s;
+        },
+        removeHash(a, s) {
+            if (s === 'area') {
+                this.areas.splice(this.areas.indexOf(a), 1);
+            } else if (s === 'keyword') {
+                this.keywords.splice(this.keywords.indexOf(a), 1);
+            }
+        },
+        createHash(s) {
+            if (s === 'area' && this.areaTag !== '') {
+                if (this.areas.length === 20) {
+                    alert('태그는 20개 이상 생성할 수 없습니다.');
+                    return;
+                }
+                this.areas.push(this.areaTag);
+                this.areaTag = '';
+            } else if (s === 'keyword' && this.keywordTag !== '') {
+                if (this.keywords.length === 10) {
+                    alert('태그는 20개 이상 생성할 수 없습니다.');
+                    return;
+                }
+                this.keywords.push(this.keywordTag);
+                this.keywordTag = '';
+            }
+        },
+        findArea() {
+            this.resultAreas = [];
+            for (var i in this.areaData) {
+                if (this.areaData[i].indexOf(this.areaTag) >= 0) {
+                    this.resultAreas.push(this.areaData[i]);
+                }
+            }
+            if (this.areaTag === '') {
+                this.resultAreas = [];
+            }
+        },
+        test1() {
+            console.log('');
         },
         submit() {},
         imageFiles(e) {
@@ -109,14 +199,6 @@ export default {
         deleteImg(id, idx) {
             this.imageArr.splice(idx, 1);
             this.imageArrBase64.splice(idx, 1);
-        },
-        getMapData() {
-            console.log(getOverlays());
-        },
-        insertComment() {
-            insertComment(this.commentTitle, this.commentContent);
-            console.log('주석 달기 성공');
-            this.commentTitle = this.commentContent = '';
         },
     },
 };
